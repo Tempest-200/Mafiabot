86baee5385ecd100d0e98e1a94d83b30d6d9cb8d
Tempest-200 <geetmia16@gmail.com>
2026-02-25 00:26:33 +0400

    Add Flask server to keep bot alive

---
+++ keep_alive.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from threading import Thread
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Mafia Bot is alive!"
+
+def run():
+    app.run(host="0.0.0.0", port=10000)
+
+def keep_alive():
+    server = Thread(target=run)
+    server.daemon = True
+    server.start()
